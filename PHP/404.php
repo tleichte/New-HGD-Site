0445c8ceef3b34f8eee1077a38bce8467d20dce3
Tyler Eichten <tleichte@mtu.edu>
2019-08-30 17:58:09 -0400

    Created 404 page

---
+++ PHP/404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div class="main-content-wrapper main-content-spaced editor-styled" style="text-align: center;">
+    <h1>Oops!</h1>
+    <p>
+        The page you requested could not be found. If you believe this is an error, please <a href="<?php echo get_permalink(get_page_by_title('Contact')); ?>">contact</a> us.
+    </p>
+</div>
+
+
+<?php get_footer(); ?>
